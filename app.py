01f42e3b9e369da5220c2c8c2ca443b2581e6070
RASHMI PARUTI <97661454+Rashmi-Paruti@users.noreply.github.com>
2022-09-04 19:55:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-
-
-import pandas as pd
-import pickle
-import streamlit as st
 import numpy as np
+import pickle
+import pandas as pd
 
-st.set_page_config(page_title="Book Recommendation System",)
-model=pickle.load(open('final_model.pkl','rb'))
+
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+
+
+rating_table = pickle.load(open("rating_table.pkl","rb"))
+
+
+books_name = rating_table.index.to_list()
+
+
+sparse_matrix = csr_matrix(rating_table)
+model = NearestNeighbors(algorithm='brute')
+model.fit(sparse_matrix)
+
+
+#Function for recommending books
+def recommend(book_name):
+  recommended_books = []
+  image_url = []
+  book_index = np.where(rating_table.index==book_name)[0][0]
+  distances , suggestions = model.kneighbors(rating_table.iloc[book_index,:].values.reshape(1,-1),n_neighbors=5)
+  suggestions = np.ravel(suggestions, order='C') #2d to 1d array
+  for i in suggestions:
+    recommended_books.append(rating_table.index[i])
+
+ 
+    
+  return recommended_books
